1b8dbec11731ceefdeff98184e5a7e3c7e35b64a
Wannes Pipien <wannes.pipien@student.pxl.be>
2026-03-10 10:55:31 +0100

    Added initial empty pages, controller and asp actions for navigation

---
+++ EncryptieGroep3/Controllers/SecurityController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EncryptieGroep3.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ActionName("Part 1")]
+        public IActionResult Part1()
+        {
+            return View("Part1");
+        }
+
+        [ActionName("Part 2")]
+        public IActionResult Part2()
+        {
+            return View("Part2");
+        }
+
+        [ActionName("Part 3")]
+        public IActionResult Part3()
+        {
+            return View("Part3");
+        }
+
+        [ActionName("Part 4")]
+        public IActionResult Part4()
+        {
+            return View("Part4");
+        }
+
+        [ActionName("Part 5")]
+        public IActionResult Part5()
+        {
+            return View("Part5");
+        }
+    }
+}
